cc41edb8cc2731cfc8e788deef317d01a945a778
lorenzo-mariani <marianilorenzo1362@gmail.com>
2020-04-17 14:18:30 +0200

    Changed code structure and background image

---
+++ Monkey Podcast/js/registration.js
@@ -0,0 +1,19 @@
+document.getElementById("header-btn1").addEventListener("click", function()
+                                                 {
+    document.getElementById("mod1").style.display="block";
+})
+            
+document.getElementById("canc1").addEventListener("click", function()
+                                                 {
+    document.getElementById("mod1").style.display="none";
+})
+
+document.getElementById("header-btn2").addEventListener("click", function()
+                                                 {
+    document.getElementById("mod2").style.display="block";
+})
+            
+document.getElementById("canc2").addEventListener("click", function()
+                                                 {
+    document.getElementById("mod2").style.display="none";
+})
