d17cb2f036da6b4c1780710e23558971fd03aca2
federico-marazzi <fenderico9@gmail.com>
2020-11-04 15:39:28 +0100

    Improved search looks (responsiveness not yet implemented.

--- includes/search.inc.php
+++ includes/search.inc.php
@@ -15,79 +15,22 @@
                             OR genre LIKE CONCAT('%', ?, '%')
                             OR playlist LIKE CONCAT('%', ?, '%')
                             ORDER BY podcastTitle ASC";
-            $stmt = mysqli_stmt_init($conn);
-            if (!mysqli_stmt_prepare($stmt, $query_podcasts)) {
+            $stmt_users = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt_users, $query_users)) {
                 header("Location: ./home.php?error=sqlerror");
                 exit();
             } else {
-            mysqli_stmt_bind_param($stmt, "ssss", $search_text, $search_text, $search_text, $search_text);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_store_result($stmt);
-            $stmt->bind_result($genre, $title, $img, $channel_name, $streams, $file, $playlist);
-            $rows = $stmt->num_rows;
-            if($stmt->num_rows > 0){
-                echo "
-                <div id=\"podcasts-container\">
-                    <h4>PODCASTS</h4>
-                </div>
-                <div id=\"podcast-search\">
-                    <div id=\"podcast-left-container-search\">
-                    ";
-                $count = 0;
-                while($stmt->fetch()){
-                    $half = round($rows/2);
-                    if($count != $half){
-                        echo 
-                        "<button class=\"grid-element-btn\">
-                        <div class=\"grid-element\" id=".$file.">
-                            <img src=".str_replace('../', './', $img).">
-                            <h4 id=".$channel_name.">".strtoupper(str_replace('_', ' ', $title))."</h4>
-                            <p>".$streams." STREAMS</p>
-                            <h3 id=\"channel-name-search\">".strtoupper($channel_name)."</h3>
-                        </div>
-                        </button>";
-                    } else if($count == $half){
-                        echo "</div>
-                        <div id=\"podcast-right-container-search\">
-                        <button class=\"grid-element-btn\">
-                        <div class=\"grid-element\" id=".$file.">
-                            <img src=".str_replace('../', './', $img).">
-                            <h4 id=".$channel_name.">".strtoupper(str_replace('_', ' ', $title))."</h4>
-                            <p>".$streams." STREAMS</p>
-                            <h3 id=\"channel-name-search\">".strtoupper($channel_name)."</h3>
-                        </div>
-                        </button>";
-                    } else if($count > $half){
-                        echo "</div>";
-                    }
-                    $count += 1;
-                }
-                echo "</div>
-                </div>";
-            } else {
-                echo "<div id=\"podcasts-container\">
-                    <h4>PODCASTS</h4>
-                    </div>
-                <div id=\"default-search\">
-                    <h1 id=\"empty-search\">Sorry, no results found.</h1>
-                </div>";
-            }
-
-            if (!mysqli_stmt_prepare($stmt, $query_users)) {
-                header("Location: ./home.php?error=sqlerror");
-                exit();
-            } else {
-                mysqli_stmt_bind_param($stmt, "s", $search_text);
-                mysqli_stmt_execute($stmt);
-                mysqli_stmt_store_result($stmt);
-                $stmt->bind_result($name, $views, $ch_img);
-                if($stmt->num_rows > 0){
+                mysqli_stmt_bind_param($stmt_users, "s", $search_text);
+                mysqli_stmt_execute($stmt_users);
+                mysqli_stmt_store_result($stmt_users);
+                $stmt_users->bind_result($name, $views, $ch_img);
+                if($stmt_users->num_rows > 0){
                     echo "
                     <div id=\"users-container\">USERS</h4>
                     </div>
                         <div id=\"scrollchannel\">
                         ";
-                    while($stmt->fetch()){
+                    while($stmt_users->fetch()){
                         if($ch_img != ''){
                             echo 
                             "   <div class=\"grid-element-users\">
@@ -114,8 +57,48 @@
                     </div>";
                 }
             }
+            mysqli_stmt_close($stmt_users);
+            $stmt_podcasts = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt_podcasts, $query_podcasts)) {
+                header("Location: ./home.php?error=sqlerror");
+                exit();
+            } else {
+                mysqli_stmt_bind_param($stmt_podcasts, "ssss", $search_text, $search_text, $search_text, $search_text);
+                mysqli_stmt_execute($stmt_podcasts);
+                mysqli_stmt_store_result($stmt_podcasts);
+                $stmt_podcasts->bind_result($genre, $title, $img, $channel_name, $streams, $file, $playlist);
+                $rows = $stmt_podcasts->num_rows;
+                if($stmt_podcasts->num_rows > 0){
+                    echo "
+                    <div id=\"podcasts-container\">
+                        <h4>PODCASTS</h4>
+                    </div>
+                    <div id=\"podcast-search\">
+                        <div id=\"podcast-container-search\">
+                        ";
+                    while($stmt_podcasts->fetch()){
+                        echo 
+                        "<button class=\"grid-element-btn-search\">
+                        <div class=\"grid-element\" id=".$file.">
+                            <img src=".str_replace('../', './', $img).">
+                            <h4 id=".$channel_name.">".strtoupper(str_replace('_', ' ', $title))."</h4>
+                            <p>".$streams." STREAMS</p>
+                            <h3 id=\"channel-name-search\">".strtoupper($channel_name)."</h3>
+                        </div>
+                        </button>";
+                    }
+                    echo "</div>
+                    </div>";
+                } else {
+                    echo "<div id=\"podcasts-container\">
+                        <h4>PODCASTS</h4>
+                        </div>
+                    <div id=\"default-search\">
+                        <h1 id=\"empty-search\">Sorry, no results found.</h1>
+                    </div>";
+                }
             }
-            mysqli_stmt_close($stmt);
+            mysqli_stmt_close($stmt_podcasts);
             mysqli_close($conn);
         }  else {
             echo "<div id=\"default-search\">
