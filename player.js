97f30ebda010f768290b8d3fff90a22c0f63e6bc
federico-marazzi <fenderico9@gmail.com>
2020-11-05 15:16:31 +0100

    FIxed player bugs

--- player.js
+++ player.js
@@ -112,7 +112,7 @@ slider.oninput = function() {
 $("#next-icon-btn").click(function() {
     var btn = "next";
     var podcast_title = $(this).parents()[3].children[0].children[1].children[0].innerHTML.replace(/ /g, "_").toLowerCase();
-    var podcast_channel = $(this).parents()[3].children[0].children[1].children[1].innerHTML.replace(/ /g, "_").toLowerCase();
+    var podcast_channel = $(this).parents()[3].children[0].children[1].children[1].children[0].innerHTML.replace(/ /g, "_").toLowerCase();
     $.ajax({
       url : "./includes/retrievepodcast.inc.php?title="+podcast_title+"&button="+btn+"&channel="+podcast_channel,
       dataType: "html",
@@ -135,7 +135,7 @@ $("#next-icon-btn").click(function() {
   $("#previous-icon-btn").click(function() {
     var btn = "previous";
     var podcast_title = $(this).parents()[3].children[0].children[1].children[0].innerHTML.replace(/ /g, "_").toLowerCase();
-    var podcast_channel = $(this).parents()[3].children[0].children[1].children[1].innerHTML.replace(/ /g, "_").toLowerCase();
+    var podcast_channel = $(this).parents()[3].children[0].children[1].children[1].children[0].innerHTML.replace(/ /g, "_").toLowerCase();
     $.ajax({
       url : "./includes/retrievepodcast.inc.php?title="+podcast_title+"&button="+btn+"&channel="+podcast_channel,
       dataType: "html",
